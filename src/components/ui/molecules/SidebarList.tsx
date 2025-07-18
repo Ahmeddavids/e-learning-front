4dea628046895f62117752970af77ce4b1513396
ColdDdev <colindecorce@gmail.com>
2025-07-18 22:35:22 +0100

    Fix Code

--- src/components/ui/molecules/SidebarList.tsx
+++ src/components/ui/molecules/SidebarList.tsx
@@ -11,13 +11,8 @@ export const sidebarSections = [
         title: 'LEARN',
         items: [
             { name: 'Live Class', href: '/dashboard/live-class', icon: Video, badge: 'new' },
-<<<<<<< HEAD
-            { name: 'Learning path', href: '/learning-path', icon: Compass },
-            { name: 'Assessment', href: '/assessment', icon: BookOpenCheck },
-=======
             { name: 'Learning path', href: '/dashboard/learning-path', icon: Compass },
             { name: 'Assessment', href: '/dashboard/assessment', icon: BookOpenCheck },
->>>>>>> b8e82230a4a0194a0c9cb4ac45b029b755fb2ebf
         ]
     },
     {
